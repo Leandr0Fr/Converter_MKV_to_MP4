0b051ab694522464b17335c258c0025527f71779
LeandroFr <leandro00fr@gmail.com>
2024-08-17 21:20:44 -0300

    add timer

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import os
+import threading
+import time
 
 import ffmpeg
-from dotenv import load_dotenv
 from colorama import Fore
+from dotenv import load_dotenv
 
 
 def convert_files():
@@ -10,7 +12,9 @@ def convert_files():
     for file in os.listdir(dir_input_files):
         dir_file = os.path.join(dir_input_files, file)
         if dir_file.lower().endswith(".mkv"):
-            convert_to_mp4(dir_file.strip())
+            th_convert_to_mp4 = threading.Thread(target=convert_to_mp4, args=(dir_file.strip(),))
+            th_convert_to_mp4.start()
+            timer(th_convert_to_mp4)
 
 
 def convert_to_mp4(mkv_file: str):
@@ -24,6 +28,18 @@ def convert_to_mp4(mkv_file: str):
         print(f"{Fore.GREEN}Finished converting {basename} to {out_name}")
     except ffmpeg.Error as e:
         print(f"{Fore.RED}Error occurred: {e}")
+
+
+def timer(th_convert_to_mp4):
+    start_time = time.time()
+
+    while th_convert_to_mp4.is_alive():
+        elapsed_time = time.time() - start_time
+        print(f"Time: {elapsed_time:.2f}", end="\r")
+        time.sleep(0.1)
+
+    elapsed_time = time.time() - start_time
+    print(f"Conversion finished! elapsed time: {elapsed_time:.2f}")
 
 
 def main():
