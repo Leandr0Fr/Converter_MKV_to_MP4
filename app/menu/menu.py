c7ec07609df336486b982215383de77d2d197db7
LeandroFr <leandro00fr@gmail.com>
2024-08-27 19:04:51 -0300

    add menu converter

--- app/menu/menu.py
+++ app/menu/menu.py
@@ -1,15 +1,17 @@
 import sys
 
+from ..ffmpeg.converter import convert
 from ..utils.console import clear_console
 from .prints import *
 
 
-def menu():
+def menu() -> None:
     print_tittle()
     print_options_menu()
     user_input = int(input("Ingrese su opción: "))
+    clear_console()
     if user_input == 1:
-        print("converter")
+        converter_menu()
     elif user_input == 2:
         print("reduce")
     elif user_input == 3:
@@ -19,10 +21,27 @@ def menu():
     elif user_input == 0:
         sys.exit()
     else:
-        default()
+        default_menu()
 
 
-def default():
+def default_menu() -> None:
     clear_console()
     print_error()
     menu()
+
+
+def converter_menu() -> None:
+    print_options_converter()
+    user_input = int(input("Ingrese su opción: "))
+    if user_input >= 1 and 7 <= user_input:
+        convert(user_input)
+    elif user_input == 0:
+        sys.exit()
+    else:
+        default_converter()
+
+
+def default_converter() -> None:
+    clear_console()
+    print_error()
+    converter_menu()
