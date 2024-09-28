bbaa9c16b491fcf585b5d333f5bcfc9b94fe6ccb
LeandroFr <leandro00fr@gmail.com>
2024-09-28 04:33:16 -0300

    delete menu

--- app/constants/folders_path.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-INPUT_FOLDER = "Input_WOLOLO"
-OUTPUT_FOLDER = "Output_WOLOLO"
-
-VIDEOS_PATH = Path(os.path.expanduser("~\\Videos"))
-INPUT_PATH = os.path.join(VIDEOS_PATH, INPUT_FOLDER)
-OUTPUT_PATH = os.path.join(VIDEOS_PATH, OUTPUT_FOLDER)
